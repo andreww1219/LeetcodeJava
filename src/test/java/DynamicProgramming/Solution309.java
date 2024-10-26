f4bfab77308b9eacb7e510f066a41e11fe7b9544
andreww1219 <138897021+AndrewWang1219@users.noreply.github.com>
2024-10-26 22:54:12 +0800

    代码随想录 动态规划 买卖股票的最佳时机 含冷冻期 含手续费

--- src/test/java/DynamicProgramming/Solution309.java
+++ src/test/java/DynamicProgramming/Solution309.java
@@ -7,24 +7,33 @@ public class Solution309 {
     @Test
     public void test() {
         int[] prices = {1, 2, 3, 0, 2};
-        System.out.println(maxProfit(prices));
+        System.out.println(maxProfit1(prices));
     }
-    public int maxProfit(int[] prices) {
+    // 合并情况动态规划
+    public int maxProfit1(int[] prices) {
         int len = prices.length;
-        int[]dp = new int[2];   // 0 未持, 1 持有
-        boolean[] frozen = new boolean[len];    // frozen[i - 1] 表示下标 i 是否被冻结
-        dp[1] = -prices[0];
-        for (int i = 1; i < len; ++i) {
-            if (dp[1] + prices[i] > dp[0]) {
-                // 卖出
-                dp[0] = dp[1] + prices[i];
-                frozen[i] = true;
-            }
-            if (!frozen[i - 1] && dp[0] - prices[i] > dp[1]) {
-                // 买入
-                dp[1] = dp[0] - prices[i];
-            }
+        int[][] dp = new int[len + 1][2];   // 0 未持, 1 持有
+        dp[1][1] = -prices[0];      // dp[i] 表示第 i 天未持有和持有的收益
+        for (int i = 2; i <= len; ++i) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
+            // 此处利用了 dp[i - 1][0] == dp[i - 2][0] 合并了冷冻期后一天买入和非冷冻期后一天买入的情况
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i - 1]);
         }
-        return dp[0];
+        return dp[len][0];
+    }
+    // 状态转移
+    public int maxProfit2(int[] prices) {
+        int len = prices.length;
+        int[][] dp = new int[len][3];   // 0 持有, 1 未持有当天未卖出, 2 未持有当天卖出
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < len; ++i) {
+            // 持有可有两个情况得到；1. 前一天已持有  2. 前一天未卖出股票，今天未处于冰冻期，今天买入
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            // 未持有当天未卖出可有两种情况得到：1. 前一天也未持有未卖出  2. 前一天未持有已卖出，今天解冻
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][2]);
+            // 未持有当天卖出只有一种情况得到：前一天持有, 今天卖出
+            dp[i][2] = dp[i - 1][0] + prices[i];
+        }
+        return Math.max(dp[len - 1][1], dp[len - 1][2]);
     }
 }
