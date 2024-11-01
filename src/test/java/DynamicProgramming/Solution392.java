46aa64e326c4cd941fd658069dddbd96aa3c3363
andreww1219 <138897021+AndrewWang1219@users.noreply.github.com>
2024-11-01 22:06:06 +0800

    代码随想录 动态规划 判断子序列 不同的子序列

---
+++ src/test/java/DynamicProgramming/Solution392.java
@@ -0,0 +1,16 @@
+package DynamicProgramming;
+
+// 判断子序列
+public class Solution392 {
+    public boolean isSubsequence(String s, String t) {
+        int len1 = s.length(), len2 = t.length();
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        int cur = 0;
+        for (int i = 0; cur < len1 && i < len2; ++i) {
+            if (arr1[cur] == arr2[i]) cur++;
+            if (cur == len1) break;
+        }
+        return cur == len1;
+    }
+}
