f08bf6354e564b2dcd64e6d4b1bf2c086bb8a83f
drewljwang <1004718908@qq.com>
2024-11-07 12:32:52 +0800

    代码随想录 单调栈 下一个更大元素II

---
+++ src/test/java/MonotonicStack/Solution503.java
@@ -0,0 +1,24 @@
+package MonotonicStack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+// 下一个更大元素 II
+public class Solution503 {
+    public int[] nextGreaterElements(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        Arrays.fill(res, -1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        for (int i = 0; i < 2 * len; ++i) {
+            while (!stack.isEmpty() && nums[i % len] > nums[stack.peek() % len]) {
+                int top = stack.pop();
+                res[top % len] = nums[i % len];
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
