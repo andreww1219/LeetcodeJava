117bb1ae904545c83bcc7e6950cc22d325c36c2c
drewljwang <1004718908@qq.com>
2024-11-22 13:00:42 +0800

    Hot100 链表 反转链表

---
+++ src/test/java/Hot100/LinkedList/Solution206.java
@@ -0,0 +1,15 @@
+package Hot100.LinkedList;
+
+// 反转链表
+public class Solution206 {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
