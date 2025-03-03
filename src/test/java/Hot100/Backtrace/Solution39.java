7528cce37c074d64932a6ecb60dbd5bd0b586af2
andreww1219 <138897021+AndrewWang1219@users.noreply.github.com>
2025-03-03 23:31:00 +0800

    Hot100 回溯 组合总和 解决冲突

--- src/test/java/Hot100/Backtrace/Solution39.java
+++ src/test/java/Hot100/Backtrace/Solution39.java
@@ -6,9 +6,25 @@ import java.util.List;
 
 // 组合总和
 public class Solution39 {
-    List<List<Integer>> result = new ArrayList<>();
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    List<List<Integer>> res = new ArrayList<>();
+    private void backtrace(int[] candidates, int target, List<Integer> arr, int sum, int start) {
+        if (start == candidates.length) return;
+        if (sum == target) {
+            res.add(new ArrayList<>(arr));
+            return;
+        }
+        // 选择
+        if (candidates[start] + sum <= target) {
+            arr.add(candidates[start]);
+            backtrace(candidates, target, arr, sum + candidates[start], start);
+            arr.remove(arr.size() - 1);
+        }
+        // 不选择
+        backtrace(candidates, target, arr, sum, start + 1);
+    }
 
-        return result;
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backtrace(candidates, target, new ArrayList<>(), 0, 0);
+        return res;
     }
 }
